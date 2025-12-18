7c0aa74c6ae2a2bacb6f505d2c7d1d45eeadb44b
krishnaveniammi <167691112+krishnaveniammi@users.noreply.github.com>
2025-12-18 14:58:10 +0530

    Implement merge sort algorithm in merge_sort.py

---
+++ searching-sorting/merge_sort.py
@@ -0,0 +1,26 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr)//2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    
+    return merge(left, right)
+
+def merge(l, r):
+    result = []
+    i = j = 0
+    while i < len(l) and j < len(r):
+        if l[i] < r[j]:
+            result.append(l[i])
+            i += 1
+        else:
+            result.append(r[j])
+            j += 1
+    result.extend(l[i:])
+    result.extend(r[j:])
+    return result
+
+nums = [2,3,6,4,8,62,54]
+print(merge_sort(nums))
