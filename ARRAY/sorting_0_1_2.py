2c2631e934d90c2d4cf9b55cfc8a3add20b38acb
krishnaveniammi <167691112+krishnaveniammi@users.noreply.github.com>
2026-03-07 14:58:42 +0530

    Implement Dutch National Flag sorting algorithm

---
+++ ARRAY/sorting_0_1_2.py
@@ -0,0 +1,16 @@
+def sorting_0_1_2(arr):
+    low = mid = 0
+    high = len(arr)-1
+    while mid <=high:
+        if arr[mid]==0:
+            arr[mid],arr[low]=arr[low],arr[mid]
+            low +=1
+            mid +=1
+        elif arr[mid] == 1:
+            mid +=1
+        else:
+            arr[mid],arr[high] = arr[high],arr[mid]
+            high -=1
+    return arr
+arr = [2,0,2,1,0,1]
+print(sorting_0_1_2(arr))
