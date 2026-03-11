ddce8c411240c067c0f6ff6dbbd30431027da7ac
krishnaveniammi <167691112+krishnaveniammi@users.noreply.github.com>
2026-03-11 11:37:51 +0530

    Add function to calculate character frequency in string

---
+++ String/char_frequency.py
@@ -0,0 +1,13 @@
+def char_frequency(s):
+    freq = {}
+
+    for ch in s:
+        if ch in freq:
+            freq[ch] += 1
+        else:
+            freq[ch] = 1
+
+    return freq
+
+
+print(char_frequency("programming"))
